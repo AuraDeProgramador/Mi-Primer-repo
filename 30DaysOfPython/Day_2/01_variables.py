3e49e527d4757ad7e5d86acf22b8d3df7d0f08a5
AuraDeProgramador <127455444+AuraDeProgramador@users.noreply.github.com>
2023-03-09 21:43:28 -0300

    agregue todo

---
+++ 30DaysOfPython/Day_2/01_variables.py
@@ -0,0 +1,12 @@
+### Day 2: 30 Days of python programming ###
+
+first_name = 'Matias'
+last_name = 'Martinez'
+full_name = first_name + last_name
+country = 'Argentina'
+city = 'Formosa'
+age = 34
+year = 2023
+
+print("First name:", first_name)
+print("First name length:", len(first_name))
